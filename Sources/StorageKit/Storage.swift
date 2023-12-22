21a7358b28cf33e5c81e4e901448868ab226596f
Lorenzo Limoli <l.limoli@reply.it>
2023-12-22 23:43:32 +0100

    Split `Storage` protocol into three smaller one based on save, load and delete operations applying ISP

--- Sources/StorageKit/Storage.swift
+++ Sources/StorageKit/Storage.swift
@@ -7,13 +7,19 @@
 
 import Foundation
 
-public protocol Storage{
+public typealias Storage = StorageSaver & StorageLoader & StorageRemover
+
+public protocol StorageSaver{
     func save(_ data: Data, withTag tag: String) throws
     func save<T: Encodable>(_ object: T, withTag tag: String) throws
-    
+}
+
+public protocol StorageLoader {
     func loadData(withTag tag: String) throws -> Data
     func loadObject<T: Decodable>(withTag tag: String) throws -> T
-    
+}
+
+public protocol StorageRemover {
     @discardableResult
     func deleteItem(withTag tag: String) -> Bool
     @discardableResult
